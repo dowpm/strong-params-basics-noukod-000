8e53398033edef71a48f6c671ac163be0570c77b
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-05 06:06:49 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,16 +16,16 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
-	 
+
 	private
-	 
-	 
+
+
 	# We pass the permitted fields in as *args;
 	# this keeps `post_params` pretty dry while
 	# still allowing slightly different behavior
